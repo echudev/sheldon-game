beb855caa400fe42d92ddf5c179b0008234b2351
Ezequiel <ezequiel.maranda@gmail.com>
2023-01-26 17:38:57 -0300

    aumento tamaño fontSize contador

--- src/components/Score/index.tsx
+++ src/components/Score/index.tsx
@@ -1,7 +1,7 @@
 const Score = () => {
   return <div className="bg-slate-200 rounded-md flex flex-col items-center justify-center h-full max-h-36 aspect-4/3 ">
     <div className="text-score text-center text-sm sm:text-base font-bold">Score</div>
-    <div className="text-gray-700 text-3xl sm:text-5xl font-bold">0</div>
+    <div className="text-gray-600 text-5xl sm:text-6xl font-bold">12</div>
   </div>;
 };
 
