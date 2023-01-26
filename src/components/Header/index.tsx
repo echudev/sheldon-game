a46ff9589496ee7d1310f4c51657167bb031cd87
Ezequiel <ezequiel.maranda@gmail.com>
2023-01-26 13:47:34 -0300

    agrego componente Score al Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import Score from "../Score";
 
 const Header = () => {
   return (
-    <div className="container flex max-w-3xl mt-12">
-      <div className="w-full p-6 flex items-center justify-between mx-10 border-4 rounded-xl border-headerOutline">
+    <div className="container flex max-w-3xl mt-10">
+      <div className="w-full py-4 px-6 flex items-center justify-between mx-8 border-4 rounded-xl border-headerOutline">
         <img
           src={logo}
           alt="logo"
-          className="flex max-h-20 md:max-h-full"
+          className="flex max-h-20 sm:max-h-full p-2 sm:p-1"
         />
         <Score />
       </div>
--- src/components/Score/Score.test.tsx
+++ src/components/Score/Score.test.tsx
@@ -9,17 +9,4 @@ describe("Accordion", () => {
   test("should show the Score at the start", () => {
     expect(screen.queryByText(/Score/i)).toBeDefined();
   });
-
-  //   test("should show the content when button open is clicked", () => {
-  //     const button = screen.getByText(/open/i);
-  //     fireEvent.click(button);
-  //     expect(screen.queryByText(/content/i)).toBeDefined();
-  //   });
-
-  //   test("should hide the content when button open is clicked twice", () => {
-  //     const button = screen.getByText(/open/i);
-  //     fireEvent.click(button);
-  //     fireEvent.click(button);
-  //     expect(screen.queryByText(/content/i)).toBeNull();
-  //   });
 });
