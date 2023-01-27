763a712ccf0f5540340691f95ea6f54be4f24b7e
Ezequiel <ezequiel.maranda@gmail.com>
2023-01-27 12:10:46 -0300

    corrijo tamaño botón Rules

--- src/components/ButtonRules/index.tsx
+++ src/components/ButtonRules/index.tsx
@@ -1,6 +1,11 @@
 const ButtonRules = () => {
   return (
-    <button className="absolute border-2 rounded-lg border-gray-400 py-2 px-10 text-lg m-12 bottom-0 desktop:right-0 hover:brightness-125 transition">
+    <button
+      className="absolute border-2 rounded-lg border-gray-400 
+      text-md py-1 px-8 m-10 
+      bottom-0 desktop:right-0 
+      hover:brightness-125 transition"
+    >
       RULES
     </button>
   );
