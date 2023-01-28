425ee48f11384fabe610cb6efbcfe9dc050d1a95
Ezequiel <ezequiel.maranda@gmail.com>
2023-01-28 19:50:42 -0300

    rulesbutton position bug fixed

--- src/components/ButtonRules/index.tsx
+++ src/components/ButtonRules/index.tsx
@@ -3,7 +3,7 @@ const ButtonRules = () => {
     <button
       className="mt-auto border-2 rounded-lg border-gray-400 
       text-md py-1 px-8 m-10 
-      bottom-0 desktop:right-0 
+      bottom-0 desktop:absolute desktop:right-0
       hover:brightness-125 transition"
     >
       RULES
