b9a54b8df44282d30a48f43099c814a310bf9bce
Ezequiel <ezequiel.maranda@gmail.com>
2023-01-29 10:27:04 -0300

    agrego 2xl:max-h-36 a score y logo, mejora aspecto en pantallas grandes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="container flex max-w-3xl mt-10 select-none">
       <div className="w-full py-4 px-6 flex items-center justify-between mx-8 border-4 rounded-xl border-headerOutline">
-        <img src={logo} alt="logo" className="flex max-h-20 p-2 sm:p-1" />
+        <img src={logo} alt="logo" className="flex max-h-20 2xl:max-h-36" />
         <Score />
       </div>
     </header>
