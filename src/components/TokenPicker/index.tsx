03bcb97fb8e68a91d7aeae8f570d595952d7bd52
Ezequiel <ezequiel.maranda@gmail.com>
2023-01-28 21:30:38 -0300

    agrego primer test del componente TokenPicker

--- src/components/TokenPicker/index.tsx
+++ src/components/TokenPicker/index.tsx
@@ -1,15 +1,9 @@
-// import pentagon from "../../assets/bg-pentagon.svg";
 // import GameToken from "../GameToken";
 
 const TokenPicker = () => {
   return (
-    <main className="flex h-full w-full max-w-3xl my-16 select-none border border-red-500">
-      <div className="bg-pentagon bg-no-repeat bg-contain bg-center h-full w-full flex flex-col">
-        {/* <img
-          src={pentagon}
-          alt="pentagon"
-          className="absolute w-full h-full object-contain"
-        /> */}
+    <main className="flex w-full max-w-3xl my-16 select-none border border-red-500 aspect-4/3">
+      <div className="bg-pentagon bg-no-repeat bg-center bg-contain relative h-full w-full flex flex-col">
         {/* <div className="relative border border-blue-400">HOla</div>
         <div className="relative border border-yellow-300">HOla</div>
         <div className="relative border border-pink-400">HOla</div> */}
---
+++ src/tests/TokenPicker/TokenPicker.test.tsx
@@ -0,0 +1,30 @@
+import TokenPicker from "../../components/TokenPicker";
+import { render, screen } from "@testing-library/react";
+
+describe("TokenPicker", () => {
+  beforeEach(() => {
+    render(<TokenPicker />);
+  });
+  test("Component must be rendered at start, while userPick is false", () => {
+    expect(screen.getByRole(/main/i)).toBeDefined();
+  });
+  test("All 5 token variants must be displayed", () => {
+    //   expect(screen.queryByText(/content/i)).toBeNull();
+  });
+  test("When userPick is true TokenPiker component must not be rendered anymore", () => {
+    // expect(screen.queryByText(/content/i)).toBeNull();
+  });
+
+  //   test("should show the content when button open is clicked", () => {
+  //     const button = screen.getByText(/open/i);
+  //     fireEvent.click(button);
+  //     expect(screen.queryByText(/content/i)).toBeDefined();
+  //   });
+
+  //   test("should hide the content when button open is clicked twice", () => {
+  //     const button = screen.getByText(/open/i);
+  //     fireEvent.click(button);
+  //     fireEvent.click(button);
+  //     expect(screen.queryByText(/content/i)).toBeNull();
+  //   });
+});
