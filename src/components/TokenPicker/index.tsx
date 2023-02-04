14856bec95ae9d4b23e2dea7b94bd68507a22f73
Ezequiel <ezequiel.maranda@gmail.com>
2023-02-04 12:58:02 -0300

    tokenpicker listo en pantallas mobile

--- src/components/TokenPicker/index.tsx
+++ src/components/TokenPicker/index.tsx
@@ -1,17 +1,27 @@
-// import GameToken from "../GameToken";
+import GameToken from "../GameToken";
 import pentagon from "../../assets/bg-pentagon.svg";
 
 const TokenPicker = () => {
   return (
-    <main className="flex flex-col items-center w-full max-w-3xl my-auto select-none border border-red-500 aspect-4/3">
-      <img
-        alt="pentagon"
-        src={pentagon}
-        className="relative border h-full border-green-500"
-      />
-      {/* <div className="relative border border-blue-400">HOla</div>
-        <div className="relative border border-yellow-300">HOla</div>
-        <div className="relative border border-pink-400">HOla</div> */}
+    <main className="flex flex-col items-center justify-center w-full max-w-3xl my-auto select-none border border-red-500 aspect-4/3">
+      <div className="relative max-h-52 mobile:max-h-96 h-full aspect-square">
+        <img alt="pentagon" src={pentagon} className="absolute w-full h-full" />
+        <div className="absolute top-[-22%] left-[calc(50%-60px)]">
+          <GameToken variant="scissors" />
+        </div>
+        <div className="absolute top-[15%] left-[calc(0%-55px)]">
+          <GameToken variant="spock" />
+        </div>
+        <div className="absolute top-[15%] left-[calc(100%-65px)]">
+          <GameToken variant="paper" />
+        </div>
+        <div className="absolute bottom-[-25%] mobile:bottom-[-13%] left-[calc(20%-60px)]">
+          <GameToken variant="lizard" />
+        </div>
+        <div className="absolute bottom-[-25%] mobile:bottom-[-13%] left-[calc(80%-60px)]">
+          <GameToken variant="rock" />
+        </div>
+      </div>
     </main>
   );
 };
